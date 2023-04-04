0d5f4677d66605ee9f4dfc06e44d9efcd9ed5986
Elena Kuzmina <kashtan.kuzmin@gmail.com>
2023-04-04 10:15:04 +0300

    Добавьте файлы проекта.

---
+++ ClassStudent.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppClassFirst
+{
+    internal class ClassStudent
+    {
+        //поля
+        private string fio;
+        private DateTime birthday;
+       
+        private double scholarship;
+        // свойства для доступа к полям
+        public string FIO
+        {
+            get { return fio; }//возвращает значение поля
+            set { fio = value; } //устанавливает значение поля
+        }
+               
+        public double Scholarship
+        {
+            get { return scholarship; }
+            set { scholarship = value; }
+        }
+        public DateTime Birthday
+        {
+            get { return birthday; }
+            set { birthday = value; }
+        }
+        
+        //методы 
+        //конструктор без параметров задает значения полей по умолчанию
+        public ClassStudent()
+        {
+            fio = "";
+            birthday = new DateTime(2001, 01, 01);
+            scholarship = 0.00;
+        }
+        //конструктор с параметрами
+        public ClassStudent(string f, DateTime d, double s)
+        {
+            fio = f;
+            birthday = d;
+            scholarship = s;
+        }
+        //метод вывода на экран
+        public string PrintToScreen()
+        {
+           string line = $"Студент {fio} дата рождения {birthday}" +"\n";
+           line += $"Получает стипендию в размере {scholarship} рублей"+"\n";
+
+            return line;
+        }
+        public void SaveToFile(string filename)
+        {
+            StreamWriter streamWriter = new StreamWriter(filename, true, Encoding.Default);
+            streamWriter.WriteLine("Личные данные студента");
+            streamWriter.WriteLine($"ФИО: {fio}");
+            streamWriter.WriteLine($"Дата рождения: {birthday.ToString()}");
+            streamWriter.WriteLine($"Стипендия: {scholarship.ToString()}");
+            streamWriter.Close();
+        }
+   
+       
+    }
+}
